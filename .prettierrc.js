48f82272268521ac7ad143f383a30b0517d6637e
Luca Stauble <luca.stauble@gmail.com>
2025-03-11 18:18:01 +0100

    refactor: restored original prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,5 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 module.exports = {
-  ...require("@zextras/carbonio-ui-configs/rules/prettier.js"),
-  tabWidth: 18,
+	...require('@zextras/carbonio-ui-configs/rules/prettier.js')
 };
