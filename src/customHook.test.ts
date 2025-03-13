af688ba0cd89bf9ed33bf9ee81017736bd28a3d5
Luca Stauble <luca.stauble@gmail.com>
2025-03-13 17:14:03 +0100

    test: setup vitest configuration

--- src/customHook.test.ts
+++ src/customHook.test.ts
@@ -3,24 +3,25 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-// /*
-//  * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
-//  *
-//  * SPDX-License-Identifier: AGPL-3.0-only
-//  */
-// import { renderHook } from '@testing-library/react-hooks';
-//
-// import { testFn } from './customHook';
-//
-// describe('testFn', () => {
-// 	it('returns the initial state', () => {
-// 		const { result } = renderHook(() => testFn());
-// 		expect(result.current).toBe('test');
-// 	});
-//
-// 	it('logs the correct message', () => {
-// 		console.log = jest.fn();
-// 		renderHook(() => testFn());
-// 		expect(console.log).toHaveBeenCalledWith('test');
-// 	});
-// });
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { renderHook } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+
+import { testFn } from './customHook';
+
+describe('testFn', () => {
+	it('returns the initial state', () => {
+		const { result } = renderHook(() => testFn());
+		expect(result.current).toBe('test');
+	});
+
+	it('logs the correct message', () => {
+		console.log = vi.fn();
+		renderHook(() => testFn());
+		expect(console.log).toHaveBeenCalledWith('test');
+	});
+});
